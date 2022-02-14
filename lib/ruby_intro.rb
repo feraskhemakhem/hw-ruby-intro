8fffe46f9ff79d09826cd7f8de70c1c6c4a02d74
feraskhemakhem <feraskhemakhem@gmail.com>
2022-02-14 05:36:31 +0000

    part 1 of the assignment complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,32 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  # sums the contents of an array
+  sum = 0
+  arr.each { |v| sum += v }
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if (arr.empty?()) # if empty array
+    return 0
+  elsif arr.length == 1  # if length 1, return that element
+    return arr[0]
+  else # if at least 2 elements, do regular implementation
+    max_elm = arr.min()
+    max_elm2 = arr.min()
+    arr.each { |v| if v >= max_elm then max_elm2 = max_elm and max_elm = v elsif v > max_elm2 then max_elm2 = v end }
+    return max_elm+max_elm2
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  for i in 0...arr.length() # iterate through indicies
+    for j in 0...arr.length() # iterate again
+      return true if i != j and arr[i] + arr[j] == n
+    end
+  end
+  return false
 end
 
 # Part 2
